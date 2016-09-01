c4272fd39ec31a0f15a55d95bcda411b580699c6
1stavemachine <chris@1stavemachine.com>
2016-09-01 12:53:59 -0400

    name change on template

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,7 @@
-class desktop_picture (
-    $path = '/Library/Desktop Pictures/El Capitan.jpg'
-)
 {
-    mac_profiles_handler::manage { 'com.1stavemachine.config.desktop':
+    mac_profiles_handler::manage { 'dockmaster.utility_imac':
         ensure      => 'present',
-        file_source => template('desktop_picture/com.1stavemachine.config.desktop.erb'),
+        file_source => template('dock/com.1stavemachine.utilitydock.mobileconfig.erb'),
         type        => 'template',
     }
 }
