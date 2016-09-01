c0a7a9664c992f083a89df602426874091860842
1stavemachine <chris@1stavemachine.com>
2016-09-01 13:33:42 -0400

    replace

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class dock()
 {
     mac_profiles_handler::manage { 'dockmaster.utility_imac':
         ensure      => 'present',
+        replace     => 'true',
         file_source => template('dock/com.1stavemachine.utilitydock.mobileconfig.erb'),
         type        => 'template',
     }
